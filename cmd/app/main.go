f473303b6e65deb239d50d8d8009a5284f26a1a7
MaratKamalovPD <dead_island_hd@bk.ru>
2024-03-05 13:45:59 +0300

    commit test

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,6 +6,13 @@ import (
 	"github.com/go-park-mail-ru/2024_1_IMAO/internal/app"
 )
 
+//	@title      YULA project API
+//	@version    1.0
+//	@description  This is a server of YULA server.
+//
+// @Schemes http
+// @host  109.120.183.3:8080
+// @BasePath  /api/v1
 func main() {
 	srv := new(app.Server)
 
