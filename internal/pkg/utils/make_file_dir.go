8384c7c9dfd142022e2f780ea845e756107c82c9
IlyaChgn <chugin.ilya2004@gmail.com>
2024-04-10 07:06:41 +0300

    Hotfix

--- internal/pkg/utils/make_file_dir.go
+++ internal/pkg/utils/make_file_dir.go
@@ -9,10 +9,10 @@ import (
 	"time"
 )
 
-func WriteFile(file *multipart.FileHeader, folderName string) error {
+func WriteFile(file *multipart.FileHeader, folderName string) (string, error) {
 	uploadedFile, err := file.Open()
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer uploadedFile.Close()
 
@@ -23,20 +23,21 @@ func WriteFile(file *multipart.FileHeader, folderName string) error {
 
 	err = os.MkdirAll(dirName, os.ModePerm)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	extension := filepath.Ext(file.Filename)
 	filename := RandString(8) + extension
-	destination, err := os.Create(dirName + "/" + filename)
+	fullpath := dirName + "/" + filename
+	destination, err := os.Create(fullpath)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer destination.Close()
 
 	if _, err := io.Copy(destination, uploadedFile); err != nil {
-		return err
+		return "", err
 	}
 
-	return nil
+	return fullpath, nil
 }
