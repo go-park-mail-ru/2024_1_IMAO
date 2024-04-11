dc379b8ab0385f05fd3ee1410aa4340955302df2
MaratKamalovPD <dead_island_hd@bk.ru>
2024-04-11 20:08:24 +0300

    final changes

--- db/migrations/types.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE order_status AS ENUM ('В корзине', 'В обработке', 'Оплачено', 'Закрыт');
-CREATE TYPE advert_status AS ENUM ('Скрыто', 'Удалено', 'Активно', 'Продано');
-CREATE TYPE complaint_type AS ENUM ('Спам', 'Мошенничество', 'Запрещённые материалы', 'Продажа запрещенных товаров', 'Другое');
--- db/migrations/view.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.view
-(
-    user_id         BIGINT                                              NOT NULL REFERENCES public."user" (id),
-    advert_id      BIGINT                                               NOT NULL REFERENCES public.advert (id) ON DELETE CASCADE,
-    CONSTRAINT view_uniq_together_advert_id_user_id unique (user_id, advert_id)
-);
