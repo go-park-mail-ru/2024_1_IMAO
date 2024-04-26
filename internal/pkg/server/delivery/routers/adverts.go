8523abfbdd49fe7fa32feca3f961c473e1364d66
IlyaChgn <chugin.ilya2004@gmail.com>
2024-04-27 01:36:29 +0300

    Fixed ads routes

--- internal/pkg/server/delivery/routers/adverts.go
+++ internal/pkg/server/delivery/routers/adverts.go
@@ -9,14 +9,6 @@ func ServeAdvertsRouter(router *mux.Router, advertsHandler *delivery.AdvertsHand
 	authCheckMiddleware, csrfMiddleware mux.MiddlewareFunc) {
 	subrouter := router.PathPrefix("/adverts").Subrouter()
 
-	subrouter.HandleFunc("/", advertsHandler.GetAdsList)
-	subrouter.HandleFunc("/{city:[a-zA-Z_]+}", advertsHandler.GetAdsList)
-	subrouter.HandleFunc("/{city:[a-zA-Z_]+}/{category:[a-zA-Z_]+}", advertsHandler.GetAdsList)
-	subrouter.HandleFunc("/{city:[a-zA-Z_]+}/{category:[a-zA-Z_]+}/{id:[0-9]+}", advertsHandler.GetAdvert)
-	subrouter.HandleFunc("/{id:[0-9]+}", advertsHandler.GetAdvertByID)
-	//subrouter.HandleFunc("/api/adverts/delete/{id:[0-9]+}", advertsHandler.DeleteAdvert)
-	subrouter.HandleFunc("/close/{id:[0-9]+}", advertsHandler.CloseAdvert)
-
 	subrouterCreate := subrouter.PathPrefix("/create").Subrouter()
 	subrouterCreate.Use(authCheckMiddleware, csrfMiddleware)
 	subrouterCreate.HandleFunc("", advertsHandler.CreateAdvert).Methods("POST")
@@ -24,4 +16,12 @@ func ServeAdvertsRouter(router *mux.Router, advertsHandler *delivery.AdvertsHand
 	subrouterEdit := subrouter.PathPrefix("/edit").Subrouter()
 	subrouterEdit.Use(authCheckMiddleware, csrfMiddleware)
 	subrouterEdit.HandleFunc("", advertsHandler.EditAdvert).Methods("POST")
+
+	subrouter.HandleFunc("/", advertsHandler.GetAdsList)
+	subrouter.HandleFunc("/{city:[a-zA-Z_]+}", advertsHandler.GetAdsList)
+	subrouter.HandleFunc("/{city:[a-zA-Z_]+}/{category:[a-zA-Z_]+}", advertsHandler.GetAdsList)
+	subrouter.HandleFunc("/{city:[a-zA-Z_]+}/{category:[a-zA-Z_]+}/{id:[0-9]+}", advertsHandler.GetAdvert)
+	subrouter.HandleFunc("/{id:[0-9]+}", advertsHandler.GetAdvertByID)
+	//subrouter.HandleFunc("/api/adverts/delete/{id:[0-9]+}", advertsHandler.DeleteAdvert)
+	subrouter.HandleFunc("/close/{id:[0-9]+}", advertsHandler.CloseAdvert)
 }
