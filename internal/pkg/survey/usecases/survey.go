59a9ef1481ec8bfda423dc719aa830076344a933
MaratKamalovPD <dead_island_hd@bk.ru>
2024-04-27 12:34:43 +0300

    SurveyStorageInterface updates

--- internal/pkg/survey/usecases/survey.go
+++ internal/pkg/survey/usecases/survey.go
@@ -1,7 +1,13 @@
 package usecases
 
+import (
+	"context"
+
+	models "github.com/go-park-mail-ru/2024_1_IMAO/internal/models"
+)
+
 type SurveyStorageInterface interface {
-	SaveSurveyResults()
-	GetResults()
+	SaveSurveyResults(ctx context.Context, surveyAnswers []*models.SurveyAnswer) error
+	GetResults(ctx context.Context, userID, surveyID uint) (bool, error)
 	GetStatics()
 }
