a7e73ac4d956c0cca8926adb4fe0bf336e10102d
MaratKamalovPD <dead_island_hd@bk.ru>
2024-05-17 10:56:54 +0300

    Update priority_search.sql

--- db/priority_search.sql
+++ db/priority_search.sql
@@ -39,4 +39,30 @@ ORDER BY is_promoted DESC, id ASC
 LIMIT 20;
 
 
+=========================================
+
+WITH promoted_adverts AS (
+    SELECT id, title, description, price, is_promoted
+    FROM public.advert
+    WHERE is_promoted = TRUE
+	ORDER BY id
+	OFFSET (SELECT COUNT(*) FROM public.advert WHERE is_promoted = TRUE) * (1 / (div((SELECT COUNT(*) FROM public.advert WHERE is_promoted = TRUE), 5)) * 6)
+	LIMIT 5
+), non_promoted_adverts AS (
+    SELECT id, title, description, price, is_promoted
+    FROM public.advert
+    WHERE is_promoted = FALSE
+	ORDER BY id
+	OFFSET 15 * 6 + 5 * div((SELECT 
+    CASE 
+        WHEN (6 * 5) - (SELECT COUNT(*) FROM public.advert WHERE is_promoted = TRUE) < 0 THEN 0
+        ELSE (6 * 5) - (SELECT COUNT(*) FROM public.advert WHERE is_promoted = TRUE)
+    END), 5)
+)
+SELECT * FROM promoted_adverts
+UNION ALL
+SELECT * FROM non_promoted_adverts
+ORDER BY is_promoted DESC, id ASC
+LIMIT 20;
+
 
